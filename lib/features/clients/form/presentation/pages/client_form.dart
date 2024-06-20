1f2491c28e409a1dbe853350d65b38cbb5c2a788
Mutelembi Simataa <tanyamutelembi@gmail.com>
2024-06-20 15:48:03 +0200

    chore: Update client form layout and add save client button

--- lib/features/clients/form/presentation/pages/client_form.dart
+++ lib/features/clients/form/presentation/pages/client_form.dart
@@ -1,6 +1,7 @@
 import 'package:binary_city/core/utils/utils.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/constants/colors.dart';
 import '../../../../../core/constants/constants.dart';
 import '../../../../../core/widgets/text_widget.dart';
 import '../widgets/save_client_button.dart';
@@ -54,10 +55,20 @@ class _ClientFormState extends State<ClientForm> {
               ),
             ],
           ),
+          const SizedBox(height: 20),
           //table
           Column(
             children: [
               Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                height: 60,
+                decoration: const BoxDecoration(
+                  color: AppColors.primaryColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)),
+                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
